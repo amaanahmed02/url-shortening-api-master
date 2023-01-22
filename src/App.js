e08719822076129e69ba5f84937daa5d5a538d3f
amaanahmed02 <amaan.ahmed460@gmail.com>
2023-01-23 01:44:13 +0530

    Shortly

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+
+document.title = "Shortly";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
